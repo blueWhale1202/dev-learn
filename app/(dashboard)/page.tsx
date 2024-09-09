b191f38a9b4f2c072883d1f6c9d4e53af67473cc
bluewhale <lqtu12369@gmail.com>
2024-09-09 21:21:29 +0700

    feat: authentication

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function DashboardPage() {
+    return (
+        <div>
+            Dashboard Page
+            <UserButton />
+        </div>
+    );
+}
