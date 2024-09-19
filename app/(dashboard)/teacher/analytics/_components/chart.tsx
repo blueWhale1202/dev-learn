81a7f925da9d4e303ef440fc186ba170e2570f10
bluewhale <lqtu12369@gmail.com>
2024-09-20 01:23:10 +0700

    feat: analytic chart

---
+++ app/(dashboard)/teacher/analytics/_components/chart.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import {
+    Bar,
+    BarChart,
+    CartesianGrid,
+    LabelList,
+    XAxis,
+    YAxis,
+} from "recharts";
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { ChartConfig, ChartContainer } from "@/components/ui/chart";
+
+const chartConfig = {
+    course: {
+        color: "hsl(var(--chart-1))",
+        label: "Course",
+    },
+} satisfies ChartConfig;
+
+type Props = {
+    data: {
+        name: string;
+        total: number;
+    }[];
+};
+
+export const Chart = ({ data }: Props) => {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Revenue Analysis</CardTitle>
+                <CardDescription>All your course</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <ChartContainer config={chartConfig}>
+                    <BarChart
+                        accessibilityLayer
+                        data={data}
+                        margin={{
+                            top: 20,
+                        }}
+                    >
+                        <CartesianGrid vertical={false} />
+                        <XAxis
+                            dataKey="name"
+                            tickLine={false}
+                            tickMargin={10}
+                            axisLine={false}
+                        />
+                        <YAxis
+                            tickLine={false}
+                            axisLine={false}
+                            tickFormatter={(value) => `$${value}`}
+                        />
+
+                        <Bar
+                            dataKey="total"
+                            fill="var(--color-course)"
+                            radius={8}
+                        >
+                            <LabelList
+                                position="top"
+                                offset={12}
+                                className="fill-foreground"
+                                fontSize={12}
+                                formatter={(value: string) => `$${value}`}
+                            />
+                        </Bar>
+                    </BarChart>
+                </ChartContainer>
+            </CardContent>
+        </Card>
+    );
+};
--- app/(dashboard)/teacher/analytics/page.tsx
+++ app/(dashboard)/teacher/analytics/page.tsx
@@ -1,3 +1,31 @@
-export default function AnalyticsPage() {
-    return <div>Analytics Page</div>;
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+import { getAnalytics } from "@/actions/get-analytics";
+
+import { Chart } from "./_components/chart";
+import { DataCard } from "./_components/data-card";
+
+export default async function AnalyticsPage() {
+    const { userId } = auth();
+
+    if (!userId) {
+        redirect("/");
+    }
+
+    const { data, totalSales, totalRevenues } = await getAnalytics(userId);
+    return (
+        <div className="p-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+                <DataCard
+                    label="Total Revenues"
+                    value={totalRevenues}
+                    shouldFormat
+                />
+                <DataCard label="Total Sales" value={totalSales} />
+            </div>
+
+            <Chart data={data} />
+        </div>
+    );
 }
